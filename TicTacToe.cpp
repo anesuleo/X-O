6fd06581145bca4bc97a33aa98a4236c6a4cd05c
anesu <anesu@LAPTOP-ARCCDNQB>
2023-03-09 21:36:56 +0000

    Add project files.

---
+++ TicTacToe.cpp
@@ -0,0 +1,57 @@
+//ANESU NJOVE, NATALIE CHIYAKA, GODSENT IDGE
+// TIC TAC TOE
+
+#include <stdio.h>
+
+int main()
+{
+	const int ROW, COL;
+	char arr[ROW][COL];
+	char* p1 = &arr[0][0];
+	char x = "X";
+	int a;
+
+	printf("Player 1 uses the X marker\n");
+	printf("Player 2 uses the O marker\n");
+	
+	printf("Choose your marker's location from the following grid\n");
+	printf("1"|"\t2"|"\t3"|"\n)
+
+
+	for (i = 1; i <= 9; i++ );
+	{
+		if (i % 2 == 1);
+		{
+			
+			printf("Player 1's turn:");
+			printf("Enter location 1: ");
+			scanf("%i", x);
+			
+			if (a == 1)
+				*p1 = x;
+			if (a == 2)
+				*p1 + 1 = x;
+			if (a == 3)
+				*p1 + 2 = x;
+			if (a == 4)
+				*p1 + 3 = x;
+			if (a == 5)
+				*p1 + 4 = x;
+			if (a == 6)
+				*p1 + 5 = x;
+			if (a == 7)
+				*p1 + 6 = x;
+			if (a == 8)
+				*p1 + 7 = x;
+			if (a == 9)
+				*p1 + 8 = x;
+
+		}
+
+		printf("%")
+		for (i = 0; i < 3)
+		{
+
+		}
+	}
+}
